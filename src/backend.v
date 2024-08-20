6e6f4e8b5588ef82cc5e0d41a53df58f0a753b0f
its-popplewell <39974659+its-popplewell@users.noreply.github.com>
2024-08-20 17:13:35 -0400

    EVENTS

--- src/backend.v
+++ src/backend.v
@@ -5,10 +5,35 @@ import sokol.gfx
 import stbi
 import math
 import os
+import gg
 
 #include "@VMODROOT/simple_shader.h" # # It should be generated with `v shader .`
 
 fn C.simple_shader_desc(gfx.Backend) &gfx.ShaderDesc
+
+///
+// STRUCTURES
+///
+
+pub struct App {
+pub mut:
+	// GG stuff?
+	gg		&gg.Context = unsafe { nil }
+	texture         gfx.Image
+	sampler         gfx.Sampler
+
+	// GLSL Pipeline
+	pipeline gfx.Pipeline
+	bind            gfx.Bindings
+
+	// My Datums
+	num_indices     int
+	width           int
+	height          int
+
+	// IMPORTANT STATE
+	is_init		bool=false
+}
 
 pub struct Vertex_Data {
 	// position
@@ -27,6 +52,144 @@ pub struct Vertex_Data {
 	typeid i16
 	id     i16
 }
+
+///
+// WINDOW FUNCTIONS
+///
+
+pub fn app_init(mut app App) {
+	desc := sapp.create_desc()
+	gfx.setup(&desc)
+
+	// APP TEXTURE SHIT
+	app.texture, app.sampler = create_texture('testimg.png')
+
+	// VERTEX AND INDEX BUFFERS
+	mut vertices, mut indices := generate_grid(app.height, app.width, 40)
+
+	// TODO: make better player code?
+	dx := [0, 40]
+	dy := [0, 72]
+	mut player := []Vertex_Data{}
+	for i in 0 .. 2 {
+		for j in 0 .. 2 {
+			player_pos_x, player_pos_y := from_pixel_to_ogl(10 + dx[j], 20 + dy[i], app.height,
+				app.width)
+			player << Vertex_Data{player_pos_x, player_pos_y, 0.25, f32(100) / f32(255), f32(149) / f32(255), f32(237) / f32(255), 1.0, 0.0, 0.0, i16(1), i16(0)}
+		}
+	}
+
+	vert_len := vertices.len
+	vertices << player
+	indices << [
+		u16(0 + vert_len),
+		u16(1 + vert_len),
+		u16(3 + vert_len),
+		u16(0 + vert_len),
+		u16(3 + vert_len),
+		u16(2 + vert_len),
+	]
+
+	// println(vertices)
+
+	app.num_indices = indices.len
+
+	mut vertex_buffer_desc := gfx.BufferDesc{
+		label: c'vertices'
+	}
+	unsafe { vmemset(&vertex_buffer_desc, 0, int(sizeof(vertex_buffer_desc))) }
+
+	vertex_buffer_desc.size = usize(vertices.len * int(sizeof(Vertex_Data)))
+	vertex_buffer_desc.data = gfx.Range{
+		ptr: vertices.data
+		size: vertex_buffer_desc.size
+	}
+
+	app.bind.vertex_buffers[0] = gfx.make_buffer(&vertex_buffer_desc)
+
+	mut index_buffer_desc := gfx.BufferDesc{
+		label: c'triangle-indices'
+	}
+	unsafe { vmemset(&index_buffer_desc, 0, int(sizeof(index_buffer_desc))) }
+
+	index_buffer_desc.size = usize(indices.len * int(sizeof(u16)))
+	index_buffer_desc.@type = .indexbuffer
+	index_buffer_desc.data = gfx.Range{
+		ptr: indices.data
+		size: index_buffer_desc.size
+	}
+
+	app.bind.index_buffer = gfx.make_buffer(&index_buffer_desc)
+	// app.bind.fs.images[C.SLOT_tex] = app.texture
+	// app.bind.fs.samplers[C.SLOT_smp] = app.sampler
+
+	shader := gfx.make_shader(C.simple_shader_desc(gfx.query_backend()))
+
+	mut pipeline_desc := gfx.PipelineDesc{}
+	unsafe{ vmemset(&pipeline_desc, 0, int(sizeof(pipeline_desc))) }
+
+	pipeline_desc.layout.buffers[0].stride = int(sizeof(Vertex_Data))
+	pipeline_desc.layout.attrs[C.ATTR_vs_position].format = .float3
+	pipeline_desc.layout.attrs[C.ATTR_vs_color0].format = .float4
+	pipeline_desc.layout.attrs[C.ATTR_vs_texcoord].format = .float2
+	pipeline_desc.layout.attrs[C.ATTR_vs_id].format = .short2
+
+	pipeline_desc.depth = gfx.DepthState {
+		write_enabled:	true
+		compare:	.less_equal
+
+	}
+	pipeline_desc.cull_mode = .back
+
+	pipeline_desc.index_type = .uint16
+	pipeline_desc.shader = shader
+
+	pipeline_desc.label = c'triangle-pipeline'
+
+	app.pipeline = gfx.make_pipeline(&pipeline_desc)
+	app.is_init = true
+}
+
+pub fn app_eventman(mut ev gg.Event, mut app App) {
+	println(ev)
+}
+
+pub fn app_frame(mut app App) {
+	if !app.is_init {
+		return
+	}
+
+	//ws := gg.window
+
+	// CLEAR CLEAR CLEAR CLEAR
+	color_action := gfx.ColorAttachmentAction {
+		// for future me: idk what the comment is to the right, but it is from ex 02 from sokol
+		load_action: unsafe { gfx.LoadAction(C.SG_LOADACTION_DONTCARE) } // C.SG_ACTION_CLEAR
+		clear_value: gfx.Color {
+			r: 1.0
+			g: 1.0
+			b: 1.0
+			a: 1.0
+		}
+	}
+	mut pass_action := gfx.PassAction{}
+	pass_action.colors[0] = color_action
+	pass := sapp.create_default_pass(pass_action)
+	gfx.begin_pass(&pass) {
+		gfx.apply_pipeline(app.pipeline)
+		gfx.apply_bindings(&app.bind)
+
+		gfx.draw(0, app.num_indices, 1)
+	}
+
+	gfx.end_pass()
+	gfx.commit()
+
+}
+
+///
+// HELPER METHODS
+///
 
 pub fn from_pixel_to_ogl(x int, y int, h int, w int) (f32, f32) {
 	normx := f32(x) / f32(w)
@@ -73,7 +236,7 @@ pub fn generate_grid(height int, width int, unitsize int) ([]Vertex_Data, []u16)
 				for j in 0 .. 2 {
 					normx := f32(corners_x[j]) / f32(width)
 					normy := 1 - (f32(corners_y[i]) / f32(height))
-					//println('Norms: ${normx} ${normy} XY: ${x} ${y} CXY: ${cx} ${cy}')
+					// println('Norms: ${normx} ${normy} XY: ${x} ${y} CXY: ${cx} ${cy}')
 					corners_ogl_x << (normx * 2) - 1
 					corners_ogl_y << (normy * 2) - 1
 				}
@@ -112,17 +275,12 @@ pub fn generate_grid(height int, width int, unitsize int) ([]Vertex_Data, []u16)
 		}
 	}
 
-	//println(outp_vert)
+	// println(outp_vert)
 	return outp_vert, outp_indx
 }
 
-fn new_vertex_data(x f32, y f32, z f32, r f32, g f32, b f32, u f32, v f32, tid i16, id i16) Vertex_Data {
-	assert 0 <= u && u <= 1
-	assert 0 <= v && v <= 1
 
-	return Vertex_Data{x, y, z, r, g, b, 1.0, u, v, tid, id}
-}
-
+// Texture!!
 fn create_texture(imgpath string) (gfx.Image, gfx.Sampler) {
 	assert os.exists(imgpath)
 
@@ -156,162 +314,4 @@ fn create_texture(imgpath string) (gfx.Image, gfx.Sampler) {
 	sg_smp := gfx.make_sampler(&smp_desc)
 
 	return sg_img, sg_smp
-}
-
-pub struct App {
-	pass_action gfx.PassAction
-mut:
-	num_indices     int
-	width           int
-	height          int
-	texture         gfx.Image
-	sampler         gfx.Sampler
-	shader_pipeline gfx.Pipeline
-	bind            gfx.Bindings
-}
-
-pub fn (mut a App) run() {
-	title := 'V Simple Shader?'
-	desc := sapp.Desc{
-		width: a.width
-		height: a.height
-		user_data: a
-		init_userdata_cb: init
-		frame_userdata_cb: frame
-		window_title: title.str
-		html5_canvas_name: title.str
-		cleanup_userdata_cb: cleanup
-		sample_count: 4
-	}
-
-	sapp.run(&desc)
-}
-
-pub fn get_default_passaction() gfx.PassAction {
-	return gfx.create_clear_pass_action(0.0, 0.0, 0.0, 1.0)
-}
-
-fn init(user_data voidptr) {
-	mut app := unsafe { &App(user_data) }
-	mut desc := sapp.create_desc()
-
-	gfx.setup(&desc)
-
-	app.texture, app.sampler = create_texture('testimg.png')
-
-	// vertices := [
-	//	Vertex_Data{-0.5, 0.5, 0.5, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0},
-	//	Vertex_Data{0.5, -0.5, 0.5, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0},
-	//	Vertex_Data{-0.5, -0.5, 0.5, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0},
-	//	Vertex_Data{0.5, 0.5, 0.5, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0},
-	//]
-
-	// indices := [
-	//	u16(0),
-	//	3,
-	//	1,
-	//	0,
-	//	1,
-	//	2,
-	//]
-
-	mut vertices, mut indices := generate_grid(app.height, app.width, 40)
-
-	dx := [0, 40]
-	dy := [0, 72]
-	mut player := []Vertex_Data{}
-	for i in 0 .. 2 {
-		for j in 0 .. 2 {
-			player_pos_x, player_pos_y := from_pixel_to_ogl(10 + dx[j], 20 + dy[i], app.height,
-				app.width)
-			player << Vertex_Data{player_pos_x, player_pos_y, 0.25, f32(100)/f32(255), f32(149)/f32(255), f32(237)/f32(255), 1.0, 0.0, 0.0, i16(1), i16(0)}
-		}
-	}
-
-	vert_len := vertices.len
-	vertices << player
-	indices << [
-		u16(0 + vert_len),
-		u16(1 + vert_len),
-		u16(3 + vert_len),
-		u16(0 + vert_len),
-		u16(3 + vert_len),
-		u16(2 + vert_len),
-
-		//u16(0), //+ u16(indices.len),
-		//u16(1), //+ u16(indices.len),
-		//u16(3), //+ u16(indices.len),
-		//u16(0), //+ u16(indices.len),
-		//u16(3), //+ u16(indices.len),
-		//u16(2), //+ u16(indices.len),
-	]
-
-	//println(vertices)
-
-	app.num_indices = indices.len
-
-	mut vertex_buffer_desc := gfx.BufferDesc{
-		label: c'vertices'
-	}
-	unsafe { vmemset(&vertex_buffer_desc, 0, int(sizeof(vertex_buffer_desc))) }
-
-	vertex_buffer_desc.size = usize(vertices.len * int(sizeof(Vertex_Data)))
-	vertex_buffer_desc.data = gfx.Range{
-		ptr: vertices.data
-		size: vertex_buffer_desc.size
-	}
-
-	app.bind.vertex_buffers[0] = gfx.make_buffer(&vertex_buffer_desc)
-
-	mut index_buffer_desc := gfx.BufferDesc{
-		label: c'triangle-indices'
-	}
-	unsafe { vmemset(&index_buffer_desc, 0, int(sizeof(index_buffer_desc))) }
-
-	index_buffer_desc.size = usize(indices.len * int(sizeof(u16)))
-	index_buffer_desc.@type = .indexbuffer
-	index_buffer_desc.data = gfx.Range{
-		ptr: indices.data
-		size: index_buffer_desc.size
-	}
-
-	app.bind.index_buffer = gfx.make_buffer(&index_buffer_desc)
-	// app.bind.fs.images[C.SLOT_tex] = app.texture
-	// app.bind.fs.samplers[C.SLOT_smp] = app.sampler
-
-	shader := gfx.make_shader(C.simple_shader_desc(gfx.query_backend()))
-
-	mut pipeline_desc := gfx.PipelineDesc{}
-	unsafe { vmemset(&pipeline_desc, 0, int(sizeof(pipeline_desc))) }
-
-	pipeline_desc.shader = shader
-	pipeline_desc.layout.attrs[C.ATTR_vs_position].format = .float3
-	pipeline_desc.layout.attrs[C.ATTR_vs_color0].format = .float4
-	pipeline_desc.layout.attrs[C.ATTR_vs_texcoord].format = .float2
-	pipeline_desc.layout.attrs[C.ATTR_vs_id].format = .short2
-	// pipeline_desc.layout.attrs[C.ATTR_vs_typeid].format = .ubyte4
-	// pipeline_desc.layout.attrs[C.ATTR_vs_id].format = .ubyte4
-	pipeline_desc.index_type = .uint16
-	pipeline_desc.label = c'triangle-pipeline'
-
-	app.shader_pipeline = gfx.make_pipeline(&pipeline_desc)
-}
-
-fn cleanup(user_data voidptr) {
-	gfx.shutdown()
-}
-
-fn frame(user_data voidptr) {
-	mut app := unsafe { &App(user_data) }
-
-	pass := sapp.create_default_pass(app.pass_action)
-	gfx.begin_pass(&pass)
-
-	gfx.apply_pipeline(app.shader_pipeline)
-	gfx.apply_bindings(&app.bind)
-
-	gfx.draw(0, app.num_indices, 1)
-
-	gfx.end_pass()
-	gfx.commit()
 }
--- src/main.v
+++ src/main.v
@@ -2,12 +2,26 @@ module main
 
 import backend
 import helper
+import gg
+import gx
 
 fn main() {
-	mut app := &backend.App{
+	mut app := &backend.App{}
+
+	app.gg = gg.new_context(
 		width: 800
 		height: 800
-		pass_action: backend.get_default_passaction()
-	}
-	app.run()
+		create_window: true
+		window_title: 'V Shader Shit'
+		user_data: app
+		bg_color: gx.white
+		frame_fn: backend.app_frame
+		init_fn: backend.app_init
+		event_fn: backend.app_eventman
+	)
+
+	app.width = 800
+	app.height = 800
+
+	app.gg.run()
 }
